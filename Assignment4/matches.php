c4628f60fc9f2ed44712dc2cb6a0b29c2a66b65b
muhammadali485 <51805705+muhammadali485@users.noreply.github.com>
2020-01-08 15:42:09 -0500

    Add files via upload

---
+++ Assignment4/matches.php
@@ -0,0 +1,21 @@
+<?php include("top.html"); ?>
+
+<html>
+<body>
+<form action="matches-submit.php" method="get">
+<fieldset>
+<legend>Returning User:</legend>
+
+<ul>
+<li>
+<strong>Name:</strong>
+<input type="text" name="name" size="16" />
+</li>
+</ul>
+                        
+<input type="submit" value="View My Matches">
+</fieldset>
+</form>
+</body>
+</html>
+<?php include("bottom.html"); ?>
